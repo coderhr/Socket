07905de0b64f0814e8cbab1a3f18141ac3e9708d
coderhr <1083984839@qq.com>
2021-02-06 16:49:42 +0800

    server and client

--- server.cpp
+++ server.cpp
@@ -9,6 +9,8 @@
 #include<errno.h>
 #include<string.h>
 
+#define BUF_SIZE 1024
+
 static bool stop = false;
 static void handle_term(int sig){
 	stop = true;
@@ -41,20 +43,26 @@ int main(int argc, char* argv[]){
 
 	ret = listen(sock, backlog);
 	assert(ret != -1);
-	sleep(20);
+
 	struct sockaddr_in client;
 	socklen_t client_addrlength = sizeof(client);
 	int connfd = accept(sock, (struct sockaddr*) &client, &client_addrlength);
 	if(connfd < 0){
 		printf("errno is: %d\n", errno);
 	}else{
-		char remote[INET_ADDRSTRLEN];
-		printf("connected with ip: %s and port: %d\n", inet_ntop(AF_INET, &client.sin_addr, remote, INET_ADDRSTRLEN),
-				ntohs(client.sin_port));
+		char buffer[BUF_SIZE];
+
+		memset(buffer, '\0', BUF_SIZE);
+		ret = recv(connfd, buffer, BUF_SIZE-1, 0);
+		printf("got %d, bytes of normal data '%s'\n", ret, buffer);
+		memset(buffer, '\0', BUF_SIZE);
+		ret = recv(connfd, buffer, BUF_SIZE-1, MSG_OOB);
+	    printf("got %d bytes of oob data '%s'\n", ret, buffer);
+		memset(buffer, '\0', BUF_SIZE);
+		ret = recv(connfd, buffer, BUF_SIZE-1, 0);
+		printf("got %d bytes of normal data '%s'\n", ret, buffer);
+
 		close(connfd);
-	}
-	while(!stop){
-		sleep(1);
 	}
 	close(sock);
 	return 0;
